452d948f3f54a498ecfc6eb829b85ee5754974bf
Ezekiel Elin <ezekielelin@me.com>
2025-11-01 18:18:29 -0400

    Finalize copying code

--- src/main/java/dev/ezekiel/timekeeper/TimeKeeper.java
+++ src/main/java/dev/ezekiel/timekeeper/TimeKeeper.java
@@ -3,8 +3,16 @@ package dev.ezekiel.timekeeper;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.world.level.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
+import com.luckycatlabs.sunrisesunset.dto.Location;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class TimeKeeper implements ModInitializer {
 	public static final String MOD_ID = "time-keeper";
@@ -14,13 +22,65 @@ public class TimeKeeper implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final TimeZone tz = Calendar.getInstance().getTimeZone();
+    public static double LATITUDE = 42.26268;
+    public static double LONGITUDE = -71.80249;
+
+    private short counter = 0;
+
 	@Override
 	public void onInitialize() {
-        ServerTickEvents.END_SERVER_TICK.register((world) -> {
-            if (world.getTickCount() % 20 == 0) {
-                LOGGER.info("Time is passing.");
-                
+        ServerTickEvents.END_WORLD_TICK.register((world) -> {
+            if (world.isClientSide()) {
+                return;
             }
+
+            counter += 1;
+            if (counter > 100) {
+                counter = 0;
+
+                GameRules gr = world.getGameRules();
+                if (gr.getBoolean(GameRules.RULE_DAYLIGHT)) {
+                    gr.getRule(GameRules.RULE_DAYLIGHT).set(false, world.getServer());
+                    LOGGER.info("Daylight cycle stopped");
+                }
+
+                Location l = new Location(LATITUDE, LONGITUDE);
+                SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(l, tz.getID());
+
+                Calendar nowCal = GregorianCalendar.getInstance();
+                Calendar sunriseCal = calculator.getOfficialSunriseCalendarForDate(GregorianCalendar.getInstance());
+                Calendar sunsetCal = calculator.getOfficialSunsetCalendarForDate(GregorianCalendar.getInstance());
+
+                double sunrise = (double) (sunriseCal.get(Calendar.HOUR_OF_DAY) + ((double) sunriseCal.get(Calendar.MINUTE)) / 60);
+                double sunset = (double) (sunsetCal.get(Calendar.HOUR_OF_DAY) + ((double) sunsetCal.get(Calendar.MINUTE)) / 60);
+                double now = (double) (nowCal.get(Calendar.HOUR_OF_DAY) + ((double) nowCal.get(Calendar.MINUTE)) / 60);
+
+                int mcTime;
+                if (now < sunrise || now > sunset) {
+                    double nightLength = (24 - sunset) + (sunrise);
+                    double percentThroughNight = 0.0;
+                    if (now > sunset) {
+                        percentThroughNight = (now - sunset) / (nightLength);
+                    } else {
+                        percentThroughNight = (now + (24 - sunset)) / (nightLength);
+                    }
+                    mcTime = (int) (12000 + (12000 * percentThroughNight));
+                } else {
+                    double dayLength = sunset - sunrise;
+
+                    double percentThroughDay = (now - sunrise) / (sunset - sunrise);
+
+                    mcTime = (int) (12000 * percentThroughDay);
+                }
+
+                world.setDayTime(mcTime);
+            }
+
+//            if (world.getDayTime() % 20 == 0) {
+//                LOGGER.info("Time is passing.");
+//                world.set
+//            }
         });
 	}
 }
